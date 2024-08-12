db83039dbd0c8c52112261fd7939e10f8ad537a8
yaserantariksa <yaserantariksa@gmail.com>
2024-08-12 13:34:56 +0700

    conect new database

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,10 +79,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yaserantariksa$default',
+        'USER': 'yaserantariksa',
+        'PASSWORD': 'ysrant*1199',
+        'HOST': 'yaserantariksa.mysql.pythonanywhere-services.com',
     }
 }
 
