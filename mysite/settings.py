071b93d9abe3fc94ef576e11f3edc2f213b56056
yaserantariksa <yaserantariksa@gmail.com>
2024-08-12 14:04:20 +0700

    add env

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,10 +91,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'yaserantariksa$default',
-        'USER': 'yaserantariksa',
-        'PASSWORD': 'ysrant*1199',
-        'HOST': 'yaserantariksa.mysql.pythonanywhere-services.com',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOSTNAME'),
     }
 }
 
